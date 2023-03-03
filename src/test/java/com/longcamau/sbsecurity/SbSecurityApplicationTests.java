463c2639417afbfa3259820ae7779316920ed10c
Long Nguyen <longcamau@web.de>
2023-03-03 09:12:14 +0100

    first commit

---
+++ src/test/java/com/longcamau/sbsecurity/SbSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.longcamau.sbsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
