e7353304d6c53fc6980a1d8870c110f419519501
Long Nguyen <longcamau@web.de>
2023-03-04 09:39:29 +0100

    Create folder controller Create UserController in folder controller

---
+++ src/main/java/com/longcamau/sbsecurity/controller/UserController.java
@@ -0,0 +1,9 @@
+package com.longcamau.sbsecurity.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping
+public class UserController {
+}
